3fd5d05007816337b42ffece16bea6317b493299
ZoeHansen <47985088+ZoeHansen@users.noreply.github.com>
2023-12-12 11:37:19 -0600

    Update and rename HUMAnN_MMUPHin_DifferentialAbundance.R to R_Analysis/HUMAnN_MMUPHin_DifferentialAbundance.R

--- R_Analysis/HUMAnN_MMUPHin_DifferentialAbundance.R
+++ R_Analysis/HUMAnN_MMUPHin_DifferentialAbundance.R
@@ -1,7 +1,5 @@
 #################################################
-
 # Aim 3: MMUPHin - Exploring Differentially Abundant HUMAnN Pathways
-
 #################################################
 
 # Load libraries
@@ -13,22 +11,15 @@ library(viridis)
 library(vegan)
 
 # Load data (MMUPHin requires relative abundance/proportions)
-
 # Note: metadata should contain samples as rows and feature data should contain samples as columns
 
 # Pathway abundances
-abd.data <- read.csv('D://Manning_ERIN/ERIN_Metabolomics_AIM_THREE/HUMAnN/ERIN_pathabundance_relabd_merged_CaseFollowPairs.csv',
-                     header=TRUE)
-
-abd.data <- read.csv('D://Manning_ERIN/ERIN_Metabolomics_AIM_THREE/HUMAnN/humann3_PHYLUM_results/ERIN_pathabundance_phylum_relabd_CaseFollowPairs.csv',
-                     header=TRUE)
-
+abd.data <- read.csv('D://ERIN_pathabundance_relabd_merged_CaseFollowPairs.csv', header=TRUE)
 abd.data <- abd.data %>%
   dplyr::filter(!grepl('Case.status', ER_ID))%>%
   dplyr::filter(!grepl('Case.Follow_ID', ER_ID)) %>%
   dplyr::filter(!grepl('Pathogen', ER_ID))# %>%
-#  dplyr::filter(!grepl('UNMAPPED', ER_ID)) %>% # If we want to remove "UNMAPPED" to actually observe pathway differences
-#  dplyr::filter(!grepl('UNINTEGRATED', ER_ID)) # Remove at PHYLUM level
+#  dplyr::filter(!grepl('UNMAPPED', ER_ID)) # If we want to remove "UNMAPPED" to actually observe pathway differences
   
 # Since we are interested in extracting community total abundances for pathways (not necessarily those
 # associated with a specific taxon), we will only use rows that do not contain a " | taxon-id" delimiter
@@ -38,23 +29,18 @@ abd.data.com <- abd.data %>%
   dplyr::filter(!grepl('unclassified',ER_ID))
 
 com.paths <- abd.data.com$ER_ID
-
 abd.data.com <- as.data.frame(lapply(abd.data.com[,c(2:ncol(abd.data.com))], as.numeric))
-
 rownames(abd.data.com) <- com.paths
-
 abd.data.com <- abd.data.com[,colSums(abd.data.com !=0)>0]
 abd.data.com <- abd.data.com[rowSums(abd.data.com !=0) >0,]
 
 # Make sure to sort ER_ID by descending before importing (*insert eye roll here*)
-meta.data <- read.csv('D://Manning_ERIN/ERIN_Metabolomics_AIM_THREE/ERIN_MetaboliteAnalysis_Metadata_CaseFollowPairs_clean.csv',
-                      header=TRUE)
+meta.data <- read.csv('D://ERIN_MetaboliteAnalysis_Metadata_CaseFollowPairs_clean.csv', header=TRUE)
 
 meta.data <- meta.data %>%
   dplyr::select('ER_ID','Case.status','Pathogen','Run','Case.Follow_ID',
                 'Followup.days','Residence.type','Age.years','Hospital',
                 'Avg_GS','GenomeEquivalents','Antibiotics', 'Gender','Year')
-
 rownames(meta.data) <- meta.data$ER_ID
 
 meta.data$Antibiotics[meta.data$Antibiotics == ""] <- "No"
@@ -70,7 +56,7 @@ meta.data$Antibiotics <- factor(meta.data$Antibiotics, levels=c('No','Yes'))
 
 #### Batch Correction ####
 
-# First, we will attempt to correct for batch effects due to Sequencing Run (Microbiome Data)
+# First, we will attempt to correct for batch effects due to Sequencing Run (see Hansen et al. (2023) - Scientific Reports)
 
 fit_adjust_batch <- adjust_batch(feature_abd=abd.data.com,    # abundance matrix
                                  batch='Run',             # the batch variable requiring correction
@@ -82,7 +68,6 @@ fit_adjust_batch <- adjust_batch(feature_abd=abd.data.com,    # abundance matrix
 path_abd_adj <- fit_adjust_batch$feature_abd_adj
 
 # Perform PERMANOVA to examine variability due to Sequencing Run before and after adjustment
-
 dist_before <- vegdist(t(abd.data.com), method='bray')
 dist_after <- vegdist(t(path_abd_adj), method='bray')
 
@@ -93,9 +78,6 @@ print(fit_adonis_before)
 print(fit_adonis_after)
 
 # Note: compare amount of variability explained using the R2 measure; check for significance
-# This may depend on the resolution of taxa (e.g. phylum vs. genus vs. species)
-
-
 
 #### Meta-analytical Differential Abundance ####
 
@@ -109,8 +91,6 @@ fit_lm_meta <- lm_meta(feature_abd=abd.data.com,       # designate abundance dat
                        covariates=c(''),           # Other environmental variables to account for in the model
                        data=meta.data,             # metadata file
                        control=list(verbose=FALSE))# other specifications (if desired)
-
-#meta.data$Run <- as.numeric(meta.data$Run)
 
 # Run the model
 fit_lm_meta <- lm_meta(feature_abd=abd.data.com,       
@@ -128,7 +108,7 @@ meta_fits <- fit_lm_meta$meta_fits
 meta_fits_signif <- meta_fits %>%
   filter(qval.fdr < 0.05)         # pull out significant p-adjusted values
 
-write.csv(meta_fits_signif, 'D://Manning_ERIN/ERIN_Metabolomics_AIM_THREE/HUMAnN/humann3_PHYLUM_results/MMUPHin_DifferentialAbundance/MMUPHin_DifferentialAbundance_MetaCycPathways_UNMAPPED_UNINTEGRATEDremoved_CaseStatus_CaseFollowPairs.csv',
+write.csv(meta_fits_signif, 'D://MMUPHin_DifferentialAbundance_MetaCycPathways_UNMAPPED_UNINTEGRATEDremoved_CaseStatus_CaseFollowPairs.csv',
           row.names=FALSE)
 
 # Plot the significant findings (differential abundance)
@@ -142,105 +122,15 @@ meta_fits_signif %>%
   scale_fill_manual(values=c("cyan4","darkorchid3"),
                     labels=c("Case","FollowUp"))+
   scale_y_continuous(breaks = scales::pretty_breaks(n=10))+
-  theme(legend.title = element_text(face='bold',size=12),
-        legend.text= element_text(size=12),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_line(colour = 'black',linetype = 'dashed'),
-        panel.spacing = unit(0,'lines'),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = 'black'),
-        axis.text.y = element_text(size = 11),
-        axis.text.x = element_text(size = 11),
-        axis.title = element_text(size = 14),
-        plot.margin = unit(c(0.25,0.25,0,0.25), 'cm'))+
+  hansen_theme+
   labs(
     x = '\nMetaCyc Pathway\n',
     y = '\nCoefficient\n',
     fill = 'Health Status')
 
-
-##### Investigating Discrete Population Structure #####
-
-# For this portion, we will investigate cases and follow-ups separately
-
-case.meta <- subset(meta.data, Case.status == 'Case')
-case.abd.adj <- micro_abd_adj[, rownames(case.meta)]
-
-D_case <- vegdist(t(case.abd.adj), method='bray')
-
-
-follow.meta <- subset(meta.data, Case.status == 'FollowUp')
-follow.abd.adj <- micro_abd_adj[, rownames(follow.meta)]
-
-D_follow <- vegdist(t(follow.abd.adj), method='bray')
-
 ##### Investigating Continuous Population Structure #####
 
 # Identify variables driving continuous structure
-
-###### Cases & Follow-Ups Separate ######
-### Cases ### 
-
-case.fit_continuous <- continuous_discover(feature_abd = case.abd.adj,
-                                           batch = "Run",
-                                           data = case.meta,
-                                           control = list(var_perc_cutoff = 0.5,
-                                                          verbose = FALSE))
-
-case.loading <- data.frame(feature = rownames(case.fit_continuous$consensus_loadings),
-                           loading1 = case.fit_continuous$consensus_loadings[, 1])
-
-case.loading %>%
-  arrange(-abs(loading1)) %>%
-  slice(1:20) %>%
-  arrange(loading1) %>%
-  mutate(feature = factor(feature, levels = feature)) %>%
-  ggplot(aes(x = feature, y = loading1)) +
-  geom_bar(stat = "identity") +
-  coord_flip() +
-  ggtitle("Features with top loadings")
-
-case.mds <- cmdscale(d = D_case)
-colnames(case.mds) <- c("Axis1", "Axis2")
-as.data.frame(case.mds) %>% 
-  mutate(score1 = case.fit_continuous$consensus_scores[, 1]) %>% 
-  ggplot(aes(x = Axis1, y = Axis2, color = score1)) +
-  geom_point() +
-  coord_fixed()
-
-
-
-### Follow-Ups ###
-follow.fit_continuous <- continuous_discover(feature_abd = follow.abd.adj,
-                                             batch = "Run",
-                                             data = follow.meta,
-                                             control = list(var_perc_cutoff = 0.5,
-                                                            verbose = FALSE))
-
-follow.loading <- data.frame(feature = rownames(follow.fit_continuous$consensus_loadings),
-                             loading1 = follow.fit_continuous$consensus_loadings[, 1])
-
-follow.loading %>%
-  arrange(-abs(loading1)) %>%
-  slice(1:20) %>%
-  arrange(loading1) %>%
-  mutate(feature = factor(feature, levels = feature)) %>%
-  ggplot(aes(x = feature, y = loading1)) +
-  geom_bar(stat = "identity") +
-  coord_flip() +
-  ggtitle("Features with top loadings")
-
-follow.mds <- cmdscale(d = D_follow)
-colnames(follow.mds) <- c("Axis1", "Axis2")
-as.data.frame(follow.mds) %>% 
-  mutate(score1 = follow.fit_continuous$consensus_scores[, 1]) %>% 
-  ggplot(aes(x = Axis1, y = Axis2, color = score1)) +
-  geom_point() +
-  coord_fixed()
-
-
-
-##### Cases + Follow-Ups Together ######
 cf.fit_continuous <- continuous_discover(feature_abd = path_abd_adj,
                                          batch = "Run",
                                          data = meta.data,
@@ -261,7 +151,7 @@ cf.loading.data <- cf.loading %>%
   arrange(loading1) %>%
   mutate(feature = factor(feature, levels = feature))
 
-write.csv(cf.loading.data, 'D://Manning_ERIN/ERIN_Metabolomics_AIM_THREE/HUMAnN/humann3_PHYLUM_results/MMUPHin_DifferentialAbundance/MMUPHin_MetaCycPathways_Top30LoadingData_UNMAPPED_UNINTEGRATEDremoved_CaseStatus_CaseFollowPairs.csv',
+write.csv(cf.loading.data, 'D://MMUPHin_MetaCycPathways_Top30LoadingData_UNMAPPED_UNINTEGRATEDremoved_CaseStatus_CaseFollowPairs.csv',
           row.names=FALSE)
 
 ggplot(data=cf.loading.data, aes(x = feature, y = loading1, fill=loading1>0)) +
@@ -270,18 +160,7 @@ ggplot(data=cf.loading.data, aes(x = feature, y = loading1, fill=loading1>0)) +
   scale_fill_manual(values=c("azure4","azure3"),
                     labels=c("Case-like","FollowUp-like"))+
   scale_y_continuous(breaks = scales::pretty_breaks(n=10))+
-  theme(legend.title = element_text(face='bold',size=12),
-        legend.text= element_text(size=12),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_line(colour = 'black',linetype = 'dashed'),
-        panel.spacing = unit(0,'lines'),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = 'black'),
-        axis.text.y = element_text(size = 11),
-        axis.text.x = element_text(size = 11),
-        axis.title = element_text(size = 14),
-        plot.margin = unit(c(0.5,0.25,0,0.25), 'cm'),
-        plot.title=element_text(face='bold',hjust=0.5, vjust=2))+
+  hansen_theme+
   ggtitle("Features with Top Loadings")+
   labs(
     x = '\nMetaCyc Pathway\n',
@@ -293,7 +172,7 @@ colnames(cf.mds) <- c("Axis1", "Axis2")
 cf.mds.data <- as.data.frame(cf.mds) %>% 
   mutate(score1 = cf.fit_continuous$consensus_scores[, 1])
 
-write.csv(cf.mds.data, 'D://Manning_ERIN/ERIN_Metabolomics_AIM_THREE/HUMAnN/humann3_PHYLUM_results/MMUPHin_DifferentialAbundance/MMUPHin_MetaCycPathways_MDS_ScoringData_UNMAPPED_UNINTEGRATEDremoved_CaseStatus_CaseFollowPairs.csv',
+write.csv(cf.mds.data, 'D://MMUPHin_MetaCycPathways_MDS_ScoringData_UNMAPPED_UNINTEGRATEDremoved_CaseStatus_CaseFollowPairs.csv',
           row.names=FALSE)
 
 
@@ -314,18 +193,6 @@ ggplot(data=cf.mds.data, aes(x = Axis1, y = Axis2)) +
   scale_color_viridis(discrete = FALSE, option = 'H')+
   scale_fill_viridis(discrete = FALSE, option = 'H')+
   coord_fixed()+
-  theme(legend.title = element_text(face='bold',size=12),
-        legend.text= element_text(size=12),
-        legend.key= element_rect(fill=NA),
-        legend.key.size = unit(0.75, "cm"),
-        legend.spacing.y = unit(0.5, 'cm'),
-        panel.grid.major = element_blank(),
-        panel.spacing = unit(0,'lines'),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = 'black'),
-        axis.text.y = element_text(size = 12),
-        axis.text.x = element_text(size = 11),
-        axis.title = element_text(size = 14),
-        plot.margin = unit(c(0.25,0.25,0,0.25), 'cm'))+
+  hansen_theme+
   labs(fill = "Score",
        shape = "Health Status")
